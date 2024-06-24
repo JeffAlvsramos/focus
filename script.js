55523bb6603b980047e1a5053697f8a1d7a7f46d
jeffAlvsramos <jeffersonalvesramos2000@gmail.com>
2024-06-24 19:04:49 -0300

    focus

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('foscuButton1').addEventListener('click', function() {
+    document.getElementById('Image1').classList.toggle('fosca');
+});
+
+document.getElementById('foscuButton2').addEventListener('click', function() {
+    document.getElementById('Image2').classList.toggle('fosca');
+});
+
+document.getElementById('foscuButton3').addEventListener('click', function() {
+    document.getElementById('Image3').classList.toggle('fosca');
+});
+
+document.getElementById('foscuButton4').addEventListener('click', function() {
+    document.getElementById('Image4').classList.toggle('fosca');
+});
